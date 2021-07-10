a4bbeb1288a5226f3ff71155be0b75fb52679cd2
Roman <Romanklacko5138@gmail.com>
2021-07-10 22:52:39 +0300

    slider ready (no)

--- #src/js/_scroll.js
+++ #src/js/_scroll.js
@@ -1,4 +1,20 @@
-
-$(document).ready(function(){
-    $('.slider__wrapper').slick();
+$(document).ready(function () {
+  $(".slider__wrapper").slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../img/icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          dots: true,
+          arrows: false,
+          
+        },
+      },
+    ],
   });
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,22 @@
-
-$(document).ready(function(){
-    $('.slider__wrapper').slick();
+$(document).ready(function () {
+  $(".slider__wrapper").slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../img/icons/left.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../img/icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 575,
+        settings: {
+          dots: true,
+          arrows: false,
+          
+        },
+      },
+    ],
   });
+});
+
 
